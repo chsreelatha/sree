5009ae7f87cf350048db3c26fc6bd374b8f84bf5
chsreelatha <39751926+chsreelatha@users.noreply.github.com>
2018-07-27 23:40:46 +0530

    Add files via upload

---
+++ CSS3/CSS3_Development-master/JavaScript_Development-master/JS_Complete_Template/12_JS_Math_Date_Objects/12_JS_Math_Date_Objects.js
@@ -0,0 +1,62 @@
+
+/*
+       Math Object Examples
+
+    */
+
+// Math PI and Sqrt
+    varpiValue=Math.PI;
+    console.log('The PI value is:' +piValue);
+
+    //sqrt
+var sqrt=Math.sqrt(256);
+console.log('The sqrt of 256 is:' + sqrt);
+
+// find the min of 4 numbers
+    var min=Math.min(10,50,46,89,78,96,5);
+    console.log('The min value is:' +min);
+
+// find the Max of 4 numbers
+var max=Math.max(10,50,46,89,78,96,5);
+console.log('The max value is:' +max);
+
+// find the 'x' to the power of 'y' value, ex: 2^4
+    var power=Math.pow(2,4);
+    console.log('2^4 is:' +power);
+
+// generate the random numbers from 0 to 100
+    var random=Math.round(Math.random();*100000);
+    console.log("Random value is :" +random);
+
+    document.getElementById("display").innerHTML="Random value"+value;
+/* For more details about Math Object, please have a look at
+https://developer   .mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Math
+*/
+
+/*
+    Date Object Examples
+
+ */
+
+// get today's date
+    var today=new Date();
+    console.log(today);
+
+//get date of the month 0 - 31 getDate()
+    var date=today.getDate()+"/"+(todsy.getMonth()+1)"/"+today
+
+//get day of the week 0 - 6 getDay()
+
+
+// get full day of the week using switch statement
+
+
+// Display a Digital Clock on the web page
+    function indianTime(){
+        var today=new Date();
+
+    }
+
+/* For More Details about Date() please have a look at
+    https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Date
+*/
---
+++ CSS3/CSS3_Development-master/JavaScript_Development-master/JS_Starter_Template/01_Basics_JS/01_app.js
@@ -0,0 +1,50 @@
+
+//Alert Box
+
+// confirm Box
+
+//console log
+
+//Display date on the console
+
+// Display date on the web page using DOM
+
+document.getElementById('display').innerHTML="Good Morning";
+
+var firstName='Sree';
+console.log(firstName);
+
+var lastName='Latha';
+var age=26;
+console.log(firstName +''+lastName +age);
+
+var job, is_Married;
+job='student';
+is_Married=true;
+console.log(firstName+lastName+'is a'+job+is_Married+age);
+
+age='twenty six';
+job='student';
+
+alert(firstName +'is a '+ age + 'year old ' +job + '.is she Married?'+is_Married);
+
+var lastName=prompt('what is the lastName?');
+console.log(firstName + lastName);
+
+//Basic operators
+
+var year,yearSree,yearMark;
+year=2018;
+ageSree=26;
+ageMark=33;
+
+//Math operators
+yearSree=year-ageSree;
+yearMark=year-yearMark;
+
+console.log(yearSree);
+
+console.log(year+2);
+
+console.log(year+ageSree);
+
